f66a7613cc3aef186a68ce7742f0839c256f7c11
bitesnail <rkwoal216@gmail.com>
2024-02-20 02:09:15 +0000

    feat: url연결 및 스웨거 추가

--- apop2/apiurls.py
+++ apop2/apiurls.py
@@ -1,7 +1,15 @@
-
 from django.urls import path, include
 from rest_framework.authtoken import views
+from accounts.apiviews import UserViewSet
+from rest_framework import routers
+from drf_spectacular.views import SpectacularSwaggerView, SpectacularAPIView
+
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
 
 urlpatterns = [
-    path('token/', views.obtain_auth_token)
+    path('token/', views.obtain_auth_token),
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('schema/swagger-ui', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('', include(router.urls))
 ]
