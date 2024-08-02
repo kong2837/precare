9525015460621388f2baf29d3d4d8f24bca28c6a
dmstjd1542@gmail.com <dmstjd1542@gmail.com>
2024-08-02 11:12:38 +0900

    feat: 아이디, 비밀번호 찾기에 필요한 forms 작성

--- accounts/forms.py
+++ accounts/forms.py
@@ -10,16 +10,21 @@ class MyAuthenticationForm(UserCreationForm):
 
 
 
-
 class PasswordResetRequestForm(forms.Form):
+    """비밀번호 찾기 시 입력 field
+        """
     name = forms.CharField(max_length=100, label="이름")
     username = forms.CharField(max_length=100, label="아이디")
     email = forms.EmailField(label="이메일")
 
 class VerifyCodeForm(forms.Form):
+    """인증코드 field
+        """
     verification_code = forms.CharField(max_length=8, label="인증번호")
 
 class SetPasswordForm(DjangoSetPasswordForm):
+    """새로운 비밀번호 입력
+        """
     new_password1 = forms.CharField(
         label="새 비밀번호",
         strip=False,
@@ -32,5 +37,7 @@ class SetPasswordForm(DjangoSetPasswordForm):
     )
 
 class FindUsernameForm(forms.Form):
+    """아이디 찾기 시 입력 field
+        """
     name = forms.CharField(max_length=100, label="이름")
     email = forms.EmailField(label="이메일")
