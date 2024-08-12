f2adeef64f4c31fe2968a4b6d8329bd1d453fdc6
dmstjd1542@gmail.com <dmstjd1542@gmail.com>
2024-08-12 15:49:33 +0900

    feat: radio 답변에 text 기입 기능 구현

--- survey/admins/answer_admin.py
+++ survey/admins/answer_admin.py
@@ -6,11 +6,12 @@ from survey.models import Answer
 class AnswerAdmin(admin.ModelAdmin):
     """Answer 모델 어드민
     """    
-    fields = ['description', 'value']
-    list_display = ['description', 'value']
+    fields = ['description', 'value', 'is_other']
+    list_display = ['description', 'value', 'is_other']
     list_display_links = ['description']
     search_fields = ['description']
     search_help_text = "설명을 입력하세요"
+    list_filter = ['is_other']
 
 
 admin.site.register(Answer, AnswerAdmin)
