14ddbfe16b1e767e062051c31da8c8f8338686a7
bitesnail <rkwoal216@gmail.com>
2024-10-04 15:25:48 +0900

    feature: 회원가입 시 이름 입력 추가

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,13 +1,23 @@
 from django import forms
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import SetPasswordForm as DjangoSetPasswordForm
-from django import forms
+from django.contrib.auth.forms import UserCreationForm
 
 
 class MyAuthenticationForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
-        fields = ("username", "first_name", "last_name")
+        fields = ("username",)
+        labels = {
+            "username": "아이디"
+        }
 
+    def __init__(self, *args, **kwargs):
+        super(MyAuthenticationForm, self).__init__(*args, **kwargs)
+
+        # Customizing placeholders for fields
+        self.fields['username'].widget.attrs.update({
+            'placeholder': '아이디를 입력하세요',
+            'class': 'form-control'
+        })
 
 
 class PasswordResetRequestForm(forms.Form):
@@ -18,10 +28,12 @@ class PasswordResetRequestForm(forms.Form):
     email = forms.EmailField(label="이메일(Zepp life 계정을 입력해주세요)",
                              widget=forms.EmailInput(attrs={'placeholder': '이메일'}))
 
+
 class VerifyCodeForm(forms.Form):
     """인증코드 field
         """
     verification_code = forms.CharField(max_length=8, label="인증번호")
+
 
 class SetPasswordForm(DjangoSetPasswordForm):
     """새로운 비밀번호 입력
@@ -36,6 +48,7 @@ class SetPasswordForm(DjangoSetPasswordForm):
         strip=False,
         widget=forms.PasswordInput,
     )
+
 
 class FindUsernameForm(forms.Form):
     """아이디 찾기 시 입력 field
--- huami/forms.py
+++ huami/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+
 from .models import HuamiAccount
 from .utils import HuamiAmazfit
 
@@ -7,18 +8,31 @@ class HuamiAccountCreationForm(forms.ModelForm):
     """Huami Account생성을 위한 모델 폼
     Raises:
         forms.ValidationError: 이메일이 이미 존재하거나 옳바르지 않은 계정정보인 경우 에러
-    """    
+    """
+
     class Meta:
         model = HuamiAccount
         fields = (
+            'name',
             'email',
             'password',
         )
-        
-        widgets={
+        labels = {
+            "name": "이름",
+            "email": "Zepp Life 계정 아이디",
+            "password": "Zepp Life 계정 비밀번호",
+        }
+
+        widgets = {
             'password': forms.PasswordInput()
         }
-    
+
+    def __init__(self, *args, **kwargs):
+        super(HuamiAccountCreationForm, self).__init__(*args, **kwargs)
+        self.fields['password'].widget.attrs.update({'class': 'form-control'})
+        self.fields['name'].widget.attrs.update({'class': 'form-control', 'placeholder': '홍길동'})
+        self.fields['email'].widget.attrs.update({'class': 'form-control'})
+
     def clean(self) -> dict:
         """폼에 들어온 데이터가 유효한지 검증
 
@@ -27,19 +41,19 @@ class HuamiAccountCreationForm(forms.ModelForm):
 
         Returns:
             dict: 검증을 거친 데이터 딕셔너리
-        """        
+        """
         cleaned_data = super().clean()
         email = cleaned_data.get('email')
         password = cleaned_data.get('password')
-        
-        #email이 Unique한지 확인하는 절차
+
+        # email이 Unique한지 확인하는 절차
         self.validate_unique()
-                
+
         try:
             HuamiAmazfit.is_valid(email, password)
         except ValueError:
             raise forms.ValidationError('이메일이나 비밀번호가 옳바르지 않습니다.')
-        
+
         return cleaned_data
 
 
@@ -52,6 +66,7 @@ class HuamiAccountCertificationForm(forms.ModelForm):
     class Meta:
         model = HuamiAccount
         fields = (
+            'name',
             'email',
             'password',
         )
