ef9fbc626bf534f3b20ac61224a023f51cd290f2
bitesnail <rkwoal216@gmail.com>
2024-12-04 10:21:51 +0900

    fix: 동기화 중 에러 발생 시 로깅되지 않던 버그 해결

--- jobs/views.py
+++ jobs/views.py
@@ -1,14 +1,26 @@
+import logging
+
+import requests
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
 from django.conf import settings
 
 from huami.models import HuamiAccount, HealthData
 
+logger = logging.getLogger(__name__)
+
 
 def update_health():
     users = HuamiAccount.objects.filter(research_status='ongoing')
     for user in users:
-        HealthData.create_from_sync_data(user)
+        try:
+            HealthData.create_from_sync_data(user)
+        except requests.HTTPError as e:
+            logger.error(e.response.reason)
+            logger.warning(f"{user.huami.name}님의 데이터 동기화에 문제가 발생했습니다.")
+        except Exception as e:
+            logger.error(e)
+            logger.warning(f"{user.huami.name}님의 데이터 동기화에 문제가 발생했습니다.")
 
 
 # Create your views here.
