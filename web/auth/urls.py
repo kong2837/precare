ed9eafab626276ae8c37e0745bafa3eca575027d
kong2837 <kongkong665123@naver.com>
2025-02-26 10:29:33 +0900

    로그인, 비밀번호&아이디 찾기 기능 및 UI 추가

--- web/auth/urls.py
+++ web/auth/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include   # 🔗 URL 경로를 정의하고 다른 URLconf를 포함하기 위한 모듈
 from . import views                     # 👀 현재 디렉토리의 views.py에서 뷰를 가져오기
-from .views import LoginView, ForgotId, Signup, ForgotPassword
+from .views import LoginView, ForgotId, Signup, ForgotPassword, VerifyEmailCodeId, VerifyEmailCodePassword, \
+    password_reset_confirm
 
 app_name = 'auth'
 
@@ -9,4 +10,7 @@ urlpatterns = [
     path('forgot_id/', ForgotId.as_view(), name='forgot_id'),  # 기본 페이지 연결
     path('forgot_password/', ForgotPassword.as_view(), name='forgot_password'),  # 기본 페이지 연결
     path('signup/', Signup.as_view(), name='signup'),  # 기본 페이지 연결
+    path('verify_email_code_id/', VerifyEmailCodeId.as_view(), name='verify_email_code_id'),  # 기본 페이지 연결
+    path('verify_email_code_password/', VerifyEmailCodePassword.as_view(), name='verify_email_code_password'),
+    path('password_reset/', password_reset_confirm, name='password_reset'),
 ]
--- web/auth/views.py
+++ web/auth/views.py
@@ -2,13 +2,28 @@ import csv
 import hashlib
 import json
 
+from django.contrib.auth.tokens import default_token_generator
+from django.shortcuts import get_object_or_404, redirect
+from django.shortcuts import render
+from django.core.mail import send_mail
 from django.urls import reverse_lazy
 from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView as Login
+
+from accounts.utils import generate_verification_code
+from accounts.views import create_profile_if_not_exists
+from huami.models import HuamiAccount
 from .forms import MyLoginForm
 from django.contrib import messages
 from django.views.generic import View, TemplateView, ListView, DetailView
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.http import HttpRequest, HttpResponse
+from django.http import JsonResponse
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from accounts.forms import SetPasswordForm
+from django.utils.encoding import force_str
 
 class LoginView(Login):
     """로그인을 위한 클래스 기반 뷰
@@ -24,14 +39,162 @@ class ForgotId(View):
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
 
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get('email')  # 폼에서 전달된 이메일 받기
+        # 이메일 인증번호 전송 로직 추가
+
+
+        try:
+                user = HuamiAccount.objects.get(email=email).user
+
+                create_profile_if_not_exists(user)
+
+                verification_code = generate_verification_code()
+                user.profile.verification_code = verification_code
+                user.profile.save()
+
+                request.session['reset_email'] = email
+
+                send_mail(
+                    '진통감지 및 스트레스 완화연구 사이트 인증코드',
+                    f'인증코드는 다음과 같습니다.: {verification_code}',
+                    'pregnancy_re@naver.com',
+                    [email],
+                    fail_silently=False,
+                )
+
+                messages.success(request, '이메일로 인증번호를 발송하였습니다')
+                return redirect('auth:verify_email_code_id')
+        except HuamiAccount.DoesNotExist:
+            messages.error(request, '등록되지 않은 이메일입니다.')
+            return redirect('auth:forgot_id')
+
+
 class ForgotPassword(View):
     template_name = 'auth/forgot_password.html'
 
     def get(self, request, *args, **kwargs):
         return render(request,self.template_name)
 
+    def post(self, request, *args, **kwargs):
+        email = request.POST.get('email')  # 폼에서 전달된 이메일 받기
+        username = request.POST.get('username')
+        # 이메일 인증번호 전송 로직 추가
+
+        try:
+            user = User.objects.get(username=username)
+            huami_account = HuamiAccount.objects.get(email=email)
+            create_profile_if_not_exists(user)
+            verification_code = generate_verification_code()
+            user.profile.verification_code = verification_code
+            user.profile.save()
+
+            request.session['reset_username'] = username
+
+            send_mail(
+                'Password Reset Verification Code',
+                f'Your verification code is {verification_code}',
+                'pregnancy_re@naver.com',
+                [huami_account.email],
+                fail_silently=False,
+            )
+
+            messages.success(request, '이메일로 인증번호를 발송하였습니다')
+            return redirect('auth:verify_email_code_password')
+        except User.DoesNotExist:
+            messages.error(request, '아이디가 존재하지 않습니다.')
+            return redirect('auth:forgot_password')
+        except HuamiAccount.DoesNotExist:
+            messages.error(request, '등록되지 않은 이메일입니다.')
+            return redirect('auth:forgot_password')
+
+
 class Signup(View):
     template_name = 'auth/signup.html'
 
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
+
+class VerifyEmailCodeId(View):
+    template_name='auth/verify_email_code_id.html'
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        verification_code = request.POST.get('verification_code')
+        email = request.session.get('reset_email')
+
+        if not email:
+            messages.error(request, 'No email found in session. Please start over.')
+            return redirect('auth:forgot_id')
+
+        try:
+            huami_account = HuamiAccount.objects.get(email=email)
+            from django.contrib.auth.models import User
+            user = User.objects.get(id=huami_account.user_id)
+
+            if user.profile.verification_code == verification_code:
+                return render(request, 'auth/complete_id.html', {'username': user.username})
+            else:
+                messages.error(request, '인증번호가 일치하지 않습니다.')
+
+        except HuamiAccount.DoesNotExist:
+            messages.error(request, 'Email not found in HuamiAccount.')
+        except User.DoesNotExist:
+            messages.error(request, 'User not found.')
+        return redirect('auth:verify_email_code_id')
+
+class VerifyEmailCodePassword(View):
+    template_name='auth/verify_email_code_id.html'
+    def get(self, request, *args, **kwargs):
+        return render(request, self.template_name)
+
+    def post(self, request, *args, **kwargs):
+        verification_code = request.POST.get('verification_code')
+        email = request.session.get('reset_email')
+
+        if not email:
+            messages.error(request, 'No email found in session. Please start over.')
+            return redirect('auth:forgot_id')
+
+        try:
+            huami_account = HuamiAccount.objects.get(email=email)
+            from django.contrib.auth.models import User
+            user = User.objects.get(id=huami_account.user_id)
+
+            if user.profile.verification_code == verification_code:
+                uid = urlsafe_base64_encode(force_bytes(user.pk))
+                token = default_token_generator.make_token(user)
+                return redirect('auth:password_reset', uidb64=uid, token=token)
+            else:
+                messages.error(request, '인증번호가 일치하지 않습니다.')
+
+        except HuamiAccount.DoesNotExist:
+            messages.error(request, 'Email not found in HuamiAccount.')
+        except User.DoesNotExist:
+            messages.error(request, 'User not found.')
+        return redirect('auth:verify_email_code_id')
+
+def password_reset_confirm(request, uidb64=None, token=None):
+    """새로운 비밀번호 변경 관련 코드
+     """
+    try:
+        uid = force_str(urlsafe_base64_decode(uidb64))
+        user = User.objects.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, User.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        if request.method == 'POST':
+            form = SetPasswordForm(user, request.POST)
+            if form.is_valid():
+                form.save()
+                messages.success(request, 'Your password was successfully updated!')
+                return redirect('accounts:password_reset_complete')
+        else:
+            form = SetPasswordForm(user)
+    else:
+        messages.error(request, "The reset link is no longer valid.")
+        return redirect("accounts:email_password_reset_request")
+
+    return render(request, 'auth/password_reset.html', {'form': form})
