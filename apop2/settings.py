6c9dfa6f20e33cfe1c70cf8c5221e1a385e4f8f7
kong2837 <kongkong665123@naver.com>
2025-04-15 14:22:27 +0900

    fitbit oauth_uri 수정

--- apop2/settings.py
+++ apop2/settings.py
@@ -101,7 +101,7 @@ DATABASES = {
         "NAME": "apopdb",
         "USER": "example-user",
         "PASSWORD": "example-password",
-        "HOST": "mariadb",
+        "HOST": "localhost",
         "PORT": "3306"
     },
 }
