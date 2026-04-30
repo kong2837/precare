a06b20287c2e572a0aec944c09a0729e40a89e4b
Jiji <luminous037@gmail.com>
2026-04-30 18:47:47 +0900

    modify skin temperature sync code

--- jobs/tasks.py
+++ jobs/tasks.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 from fitbit.daily_data.respiratory_rate import get_respiratory_rate_intraday
 from fitbit.daily_data.sleep_quality import get_sleep_stage
@@ -20,11 +20,12 @@ def sync_intra_data():
 
 def sync_daily_data():
     today = datetime.today().strftime("%Y-%m-%d")
+    yesterday = (datetime.today() - timedelta(days=1)).strftime("%Y-%m-%d")  # skin temp는 수면 후 다음날 생성되지만 '어젯밤 데이터'라서 어제 날짜로 조회해야 함
 
     for account in FitbitAccount.objects.all():
         print(f"🩺 {account.user.username}의 실시간 데이터 동기화 중...")
         get_sleep_stage(today, account)
         get_respiratory_rate_intraday(today, account)
         get_spo2_intraday(today, account)
-        get_skin_temperature(today, account)
+        get_skin_temperature(yesterday, account)
 
