42ec045ff7d5e2338f4b36d78f3b813728fd2554
Jiji <luminous037@gmail.com>
2026-05-18 11:41:20 +0900

    임신 주차별 추천 정보 띄우기

--- apop2/views.py
+++ apop2/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from datetime import date
+
+from information.services.pregnancy_data import PREGNANCY_DATA
+from accounts.utils import cal_gestational_week
 
 
 class MyLoginRequiredMixin(LoginRequiredMixin):
@@ -8,4 +12,24 @@ class MyLoginRequiredMixin(LoginRequiredMixin):
 
 
 class HomeView(TemplateView, MyLoginRequiredMixin):
-    template_name='main.html'
+    template_name = 'main.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        user = self.request.user
+
+        if hasattr(user, "fitbit") and user.fitbit.pregnancy_start_date:
+            pregnancy_start_date = user.fitbit.pregnancy_start_date.date()
+        else:
+            pregnancy_start_date = None
+
+        current_week = cal_gestational_week(
+            pregnancy_start_date,
+            date.today()
+        )
+
+        context["current_week"] = current_week
+        context["info"] = PREGNANCY_DATA.get(current_week)
+
+        return context
